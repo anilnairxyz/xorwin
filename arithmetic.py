d1da58b6b553aa1d9410725e3760423c11f61729
Anil Nair <anilnairxyz@gmail.com>
2019-04-26 13:57:57 +0100

    Add decimal support and negative number support

--- arithmetic.py
+++ arithmetic.py
@@ -1,4 +1,4 @@
-from random import sample, randint
+from random import sample, randint, choice
 from decimal import Decimal
 from fractions import Fraction
 from mixed_fractions import Mixed
@@ -54,13 +54,24 @@ class Operation:
 
 class Question1:
     """Framing BODMAS questions of the type a * b + c / d
+    where a, b, c, d are
+    (1) Integers
+    (2) Proper / Improper fractions
+    (3) Mixed Fractions
+    (4) Decimal numbers
 
     """
 
-    def __init__(self, operations=["/", "*", "+", "-"], operand_count=4, value_range=(1, 100)):
+    def __init__(self, operations=["/", "*", "+", "-"], operand_count=4, value_range=(0, 100), decimal_pt=0,
+                 negative=False):
         self.operations = operations
         self.operand_count = operand_count
+        self.decimal_pt = decimal_pt
+        self.negative = negative
         self.value_range = value_range
+        self.add_range = None
+        self.div_range = None
+        self.mul_range = None
         self._operation_count = self.operand_count - 1
         self._sample_operations = self.operations * (max(len(self.operations), 2) * 2)
         self.operand_seq = None
@@ -85,59 +96,98 @@ class Question1:
         self.__operand_count = operand_count
         self._operation_count = self.operand_count - 1
 
-    def frame_integer_question(self):
+    @property
+    def decimal_pt(self):
+        return self.__decimal_pt
+
+    @decimal_pt.setter
+    def decimal_pt(self, decimal_pt):
+        self.__decimal_pt = decimal_pt
+
+    @property
+    def negative(self):
+        return self.__negative
+
+    @negative.setter
+    def negative(self, negative):
+        self.__negative = negative
+
+    @property
+    def value_range(self):
+        return self.__value_range
+
+    @value_range.setter
+    def value_range(self, value_range):
+        self.__value_range = value_range
+        self._define_ranges()
+
+    def _define_ranges(self):
+        if self.decimal_pt == 1:
+            self.add_range = [Decimal(x) / 10 for x in range(self.value_range[0]*10, self.value_range[1]*10)]
+            self.mul_range = self.add_range
+            self.div_range = [x for x in self.mul_range if Decimal(x) != 0]
+        elif self.negative:
+            self.add_range = list(range(*self.value_range))
+            self.mul_range = list(range(int(-Decimal(self.value_range[1]).sqrt()),
+                                        int(Decimal(self.value_range[1]).sqrt())))
+            self.div_range = [x for x in self.mul_range if x != 0]
+        else:
+            self.add_range = list(range(*self.value_range))
+            self.mul_range = list(range(int(Decimal(self.value_range[0]).sqrt()),
+                                        int(Decimal(self.value_range[1]).sqrt())))
+            self.div_range = [x for x in self.mul_range if x != 0]
+
+    def frame_decimal_question(self):
         self.operand_seq = [None] * self.operand_count
         self.operation_seq = sample(self._sample_operations, self._operation_count)
         dividend_seq = [1]*self._operation_count
         for i, y in enumerate(self.operation_seq):
             if y == "/":
                 if not self.operand_seq[i]:
-                    self.operand_seq[i+1], self.operand_seq[i] = self._create_integer_division()
+                    self.operand_seq[i+1], self.operand_seq[i] = self._create_division()
                     dividend_seq[i] = self.operand_seq[i+1] * self.operand_seq[i]
                 else:
-                    self.operand_seq[i+1], _ = self._create_integer_division(divisor=dividend_seq[i-1])
+                    self.operand_seq[i+1], _ = self._create_division(divisor=dividend_seq[i-1])
                     dividend_seq[i] = dividend_seq[i-1] * self.operand_seq[i+1]
 
         for i, y in enumerate(self.operation_seq):
             if y == "*":
                 if not (self.operand_seq[i] or self.operand_seq[i+1]):
-                    self.operand_seq[i], self.operand_seq[i+1] = self._create_integer_multiplication()
+                    self.operand_seq[i], self.operand_seq[i+1] = self._create_multiplication()
                 elif not self.operand_seq[i]:
-                    self.operand_seq[i], _ = self._create_integer_multiplication()
+                    self.operand_seq[i], _ = self._create_multiplication()
                 elif not self.operand_seq[i+1]:
-                    self.operand_seq[i+1], _ = self._create_integer_multiplication()
+                    self.operand_seq[i+1], _ = self._create_multiplication()
 
         for i, y in enumerate(self.operation_seq):
             if y in ("+", "-"):
                 if not (self.operand_seq[i] or self.operand_seq[i+1]):
-                    self.operand_seq[i], self.operand_seq[i+1] = self._create_integer_addition()
+                    self.operand_seq[i], self.operand_seq[i+1] = self._create_addition()
                 elif not self.operand_seq[i]:
-                    self.operand_seq[i], _ = self._create_integer_addition()
+                    self.operand_seq[i], _ = self._create_addition()
                 elif not self.operand_seq[i+1]:
-                    self.operand_seq[i+1], _ = self._create_integer_addition()
+                    self.operand_seq[i+1], _ = self._create_addition()
 
         self.operand_seq.reverse()
         self.operation_seq.reverse()
         self.question = self._combine_operations()
         return self.question
 
-    def _create_integer_division(self, divisor=None):
-        div_range = (int(Decimal(self.value_range[0]).sqrt()), int(Decimal(self.value_range[1]).sqrt()))
+    def _create_division(self, divisor=None):
         if not divisor:
-            divisor = randint(*div_range)
-        quotient = randint(*div_range)
+            divisor = choice(self.div_range)
+        quotient = choice(self.div_range)
         dividend = quotient * divisor
         return Decimal(dividend), Decimal(divisor)
 
-    def _create_integer_multiplication(self):
-        mul_range = (int(Decimal(self.value_range[0]).sqrt()), int(Decimal(self.value_range[1]).sqrt()))
-        a = randint(*mul_range)
-        b = randint(*mul_range)
+    def _create_multiplication(self):
+        a = choice(self.mul_range)
+        b = choice(self.mul_range)
         return Decimal(a), Decimal(b)
 
-    def _create_integer_addition(self):
-        a = randint(*self.value_range)
-        b = randint(*self.value_range)
+    def _create_addition(self):
+        a = choice(self.add_range)
+        b = choice(self.add_range)
         return Decimal(a), Decimal(b)
 
     def _combine_operations(self):
@@ -162,8 +212,8 @@ class Question1:
     def _create_random_fractions(self, count):
         operand_seq = []
         for _ in range(count):
-            numerator = randint(*self.value_range)
-            denominator = randint(*self.value_range)
+            numerator = choice(self.mul_range)
+            denominator = choice(self.div_range)
             operand_seq.append(Fraction(numerator, denominator))
         return operand_seq
 
@@ -175,17 +225,26 @@ if __name__ == "__main__":
     arithmetic = Question1()
     while alive:
         i += 1
-        x = randint(1, 15)
-        if x % 3 == 0:
-            arithmetic.value_range = (1, 100)
+        x = randint(0, 15)
+        arithmetic.negative = True
+        if x % 4 == 0:
+            arithmetic.decimal_pt = 0
+            arithmetic.value_range = (0, 100)
             arithmetic.operand_count = 4
-            q = arithmetic.frame_integer_question()
-        elif x % 3 == 1:
-            arithmetic.value_range = (2, 13)
+            q = arithmetic.frame_decimal_question()
+        elif x % 4 == 1:
+            arithmetic.operand_count = 3
+            arithmetic.decimal_pt = 1
+            arithmetic.value_range = (0, 10)
+            q = arithmetic.frame_decimal_question()
+        elif x % 4 == 2:
+            arithmetic.decimal_pt = 0
+            arithmetic.value_range = (1, 100)
             arithmetic.operand_count = 3
             q = arithmetic.frame_fraction_question()
         else:
-            arithmetic.value_range = (2, 13)
+            arithmetic.decimal_pt = 0
+            arithmetic.value_range = (1, 100)
             arithmetic.operand_count = 3
             q = arithmetic.frame_fraction_question(mixed=True)
         question = q.sequence_str
@@ -202,7 +261,7 @@ if __name__ == "__main__":
         elif user_reply.isalpha():
             message += f"ERROR!\n"
         else:
-            if x % 3 == 0:
+            if x % 4 in (0, 1):
                 user_reply_formatted = Decimal(user_reply)
             else:
                 user_reply_formatted = Mixed(user_reply)
