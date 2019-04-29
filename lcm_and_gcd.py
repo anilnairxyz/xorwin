98e50eadeecf61d0325fe30ad3bae93886634e62
Anil Nair <anilnairxyz@gmail.com>
2019-04-29 16:01:15 +0100

    Change GCD to HCF

--- lcm_and_gcd.py
+++ lcm_and_gcd.py
@@ -2,21 +2,21 @@ from random import sample
 from clint.textui import colored, puts, prompt
 
 
-def gcd_calc(a, b):
-    """GCD of a and b using the Euclidean algorithm"""
+def hcf_calc(a, b):
+    """HCF of a and b using the Euclidean algorithm"""
     if a == 0:
         return b
-    return gcd_calc(b % a, a)
+    return hcf_calc(b % a, a)
 
 
 def lcm_calc(a, b):
-    """LCM of a and b using the GCD"""
-    return a * b // gcd_calc(a, b)
+    """LCM of a and b using the HCF"""
+    return a * b // hcf_calc(a, b)
 
 
-def lcm_gcd_query(value_range=(1, 100)):
+def lcm_hcf_query(value_range=(1, 100)):
     query_nos = sample(range(*value_range), 2)
-    return query_nos, gcd_calc(*query_nos), lcm_calc(*query_nos)
+    return query_nos, hcf_calc(*query_nos), lcm_calc(*query_nos)
 
 
 if __name__ == "__main__":
@@ -27,24 +27,24 @@ if __name__ == "__main__":
     while alive:
         i += 1
         s += 1
-        query, gcd, lcm = lcm_gcd_query((1, 50))
+        query, hcf, lcm = lcm_hcf_query((1, 50))
         message = f"{'#'*100} \n"
         message += f"QUESTION {str(i)}\n"
         message += f"{'#'*100} \n"
-        message += f"Find the GCD and LCM of: {colored.cyan('  '.join([str(x) for x in query]))}\n"
+        message += f"Find the HCF and LCM of: {colored.cyan('  '.join([str(x) for x in query]))}\n"
         puts(message)
-        question = f"The GCD is: "
-        gcd_reply = prompt.query(question)
+        question = f"The HCF is: "
+        hcf_reply = prompt.query(question)
         message = f"{'='*100} \n"
-        if gcd_reply == "exit":
+        if hcf_reply == "exit":
             message += f"BYE! BYE!\n"
             alive = False
             continue
-        elif int(gcd_reply) == gcd:
+        elif int(hcf_reply) == hcf:
             c += 1
             message += f"{colored.blue('You are RIGHT!')} => Score: {colored.blue(str(c)+' / '+str(s))}\n"
         else:
-            message += f"{colored.red('You are WRONG!')} Correct answer is: {colored.red(gcd)}"
+            message += f"{colored.red('You are WRONG!')} Correct answer is: {colored.red(hcf)}"
             message += f" => Score: {colored.blue(str(c)+' / '+str(s))}\n"
         puts(message)
         s += 1
